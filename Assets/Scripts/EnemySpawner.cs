eb6f5ee952bbd01a6920a7ae17c8ffe795f49e0d
ichuksin <i.chuksin@gmail.com>
2020-10-03 16:29:12 +0300

    Initial commit

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : ObjectsPool
+{
+    [SerializeField] private GameObject _spawnPointsConteiner;
+    [SerializeField] private GameObject _enemyPrefab;
+    [SerializeField] private float _spawnTime;
+    
+    private List<Transform> _spawnPoints = new List<Transform>();
+    private float _elapseTime = 0;
+    private int _currentPoint = 0;
+
+    private void Start()
+    {
+        Initialize(_enemyPrefab);
+        foreach(var item in _spawnPointsConteiner.GetComponentsInChildren<Transform>())
+        {
+            if (item.position != _spawnPointsConteiner.transform.position)
+                _spawnPoints.Add(item);
+        }
+    }
+
+    private void Update()
+    {
+        _elapseTime += Time.deltaTime;
+        if (_elapseTime >= _spawnTime)
+        {
+            Spawn();
+            _elapseTime = 0;
+        }
+        DisableObjectsAbroutScreen();
+    }
+    
+    private void Spawn()
+    {
+        GameObject enemy;
+        if (TryGetObject(out enemy))
+        {
+            enemy.transform.position = _spawnPoints[_currentPoint++].position;
+            enemy.SetActive(true);
+            if (_currentPoint == _spawnPoints.Count)
+                _currentPoint = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/ObjectsPool.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using System.Linq;
+
+public class ObjectsPool : MonoBehaviour
+{
+
+    [SerializeField] private GameObject _container;
+    [SerializeField] private int _count;
+
+    private List<GameObject> _pool = new List<GameObject>();
+    private Camera _camera;
+    protected void Initialize(GameObject prefab)
+    {
+        _camera = Camera.main;
+        for (int i = 0; i < _count; i++)
+        {
+            GameObject spawned = Instantiate(prefab, _container.transform);
+            spawned.SetActive(false);
+            _pool.Add(spawned);
+        }
+    }
+    protected bool TryGetObject(out GameObject result)
+    {
+        result = _pool.FirstOrDefault(gameObject => gameObject.activeSelf == false);
+        return result != null;
+    }
+    protected void DisableObjectsAbroutScreen()
+    {
+        foreach (var item in _pool)
+        {
+            if (item.activeSelf)
+            {
+                Vector3 position = _camera.WorldToViewportPoint(item.transform.position);
+                if (position.x < 0)
+                    item.SetActive(false);
+            }
+        }
+    }
+    public void ResetPool()
+    {
+        foreach(var item in _pool)
+        {
+            item.SetActive(false);
+        }
+    }
+}
